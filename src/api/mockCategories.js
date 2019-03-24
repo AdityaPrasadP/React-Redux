79042009bd7ecc6e73217ef81974c6c5cf9f1faf
AdityaPrasadP <48804953+AdityaPrasadP@users.noreply.github.com>
2019-03-24 16:48:49 -0700

    Add files via upload

---
+++ src/api/mockCategories.js
@@ -0,0 +1,22 @@
+export default [
+  {
+    id: "1",
+    name: 'Home/Kitchen'
+  },
+  {
+    id: "2",
+    name: 'Fruits'
+  },
+  {
+    id: "3",
+    name: 'Vegetables'
+  },
+  {
+    id: "4",
+    name: 'Pesronal Care'
+  },
+  {
+    id: "5",
+    name: 'Beverages'
+  }
+]
---
+++ src/api/mockPhones.js
@@ -0,0 +1,58 @@
+export default [
+  {
+    id: "1",
+    categoryId: "1",
+    name: "Rice",
+    description: "India Gate Basmati Rice - Feast Rozzana, 5 kg Pouch",
+    price: 5,
+    image: "/uploads/rice.jpg"
+  },
+  {
+    id: "2",
+    categoryId: "1",
+    name: "Water Bottle",
+    description: "Steelo Sunpet Water Bottle - Apsara / Rasna, Assorted Colour, 1 L Pack of 6",
+    price: 2,
+    image: "/uploads/bottle.jpg"
+  },
+  {
+    id: "3",
+    categoryId: "4",
+    name: "Dove Soap",
+    description: "Dove Bathing Bar Soap - Cream Beauty, 100 gm Pack of 3",
+    price: 2,
+    image: "/uploads/dove.JPG"
+  },
+  {
+    id: "4",
+    categoryId: "5",
+    name: "RED BULL",
+    description: "Red Bull Energy Drink, 250 ml Pack of 4",
+    price: 6,
+    image: "/uploads/redbull.jpg"
+  },
+  {
+    id: "5",
+    categoryId: "3",
+    name: "Tamato",
+    description: "Fresho Tomato - Hybrid, 1 kg",
+    price: 1.2,
+    image: "/uploads/tamato.jpg",
+    sizes: "1kg-$1.2 \n 500gm-$0.6"
+  },
+  {
+    id: "6",
+    categoryId: "2",
+    name: "Apple",
+    description: "Fresho Apple - Red Delicious / Washington, Regular, 4 pcs ",
+    price: 3,
+    image: "/uploads/apple.jpg",
+    cpu: "1.3GHz Apple A6",
+    camera: "8mp (3264x2448)",
+    size: "124.4mm x 59.2mm x 8.97mm (4.9 x 2.33 x 0.35)",
+    weight: "132 grams (4.7 ounces) with battery",
+    display: "4.0 326 pixel density",
+    battery: "1480 mAh",
+    memory: "16GB, 32GB and RAM 1 GB"
+  }
+]
